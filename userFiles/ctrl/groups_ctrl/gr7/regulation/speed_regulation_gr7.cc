c4963c38669f048f93ad8d315a92442b4e00bcf2
LailaHms <laila.elhamamsy94@gmail.com>
2016-12-18 22:12:03 +0100

    Update speed_regulation_gr7.cc

--- userFiles/ctrl/groups_ctrl/gr7/regulation/speed_regulation_gr7.cc
+++ userFiles/ctrl/groups_ctrl/gr7/regulation/speed_regulation_gr7.cc
@@ -11,6 +11,8 @@ NAMESPACE_INIT(ctrlGr7);
  */
 void speed_regulation(CtrlStruct *cvs, double r_sp_ref, double l_sp_ref)
 {
+	// variables declaration
+	
 	static double int_valr = 0;
 	static double int_vall = 0;
 	static double var_r;
@@ -21,7 +23,6 @@ void speed_regulation(CtrlStruct *cvs, double r_sp_ref, double l_sp_ref)
 	double kp = 50; //50
 	double ki = 250;//75
 
-	// variables declaration
 	CtrlIn *inputs;
 	CtrlOut *outputs;
 	SpeedRegulation *sp_reg;
