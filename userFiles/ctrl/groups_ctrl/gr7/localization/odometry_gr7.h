b3a99cbe9813f8e1a445c017cfa31cc0795ca5f8
alamouill <zunehd@hotmail.fr>
2016-12-18 22:17:15 +0100

    Update odometry_gr7.h
    Added header

--- userFiles/ctrl/groups_ctrl/gr7/localization/odometry_gr7.h
+++ userFiles/ctrl/groups_ctrl/gr7/localization/odometry_gr7.h
@@ -1,8 +1,9 @@
-/*! 
- * \author Group 7
- * \file odometry_gr7.h
- * \brief odometry of the robot
- */
+/* Mobile Robots Project - odometry_gr7.cc
+ * Estimate Robot Position Using Odometry
+ * Version 1.2
+ * Last Update: 8/11/16
+ * Group 7: Baumann, El-Hamamsy, Laumouille, Triquet
+ * EPFL, MT*/ 
 
 #ifndef _ODOMETRY_GR7_H_
 #define _ODOMETRY_GR7_H_ 
